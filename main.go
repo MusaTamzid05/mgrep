45fc7a7c518ad882b82f59a7a7b4dd83b161fe08
musa <tamzid101@gmail.com>
2022-03-19 02:49:18 +0600

    command line argument added.

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"mgrep/operations"
+	"flag"
+	"fmt"
+	"os"
 )
 
 
@@ -10,16 +13,47 @@ func Replace(searchDir, srcStr, dstStr, fileExt string) {
 	replacer.Run(searchDir, srcStr, dstStr)
 }
 
-func Match(searchDir, srcStr string) {
-	matcher := operations.MakeMatcher("cpp")
-	matcher.Run(".", "zet")
+func Match(searchDir, srcStr, fileExt string) {
+	matcher := operations.MakeMatcher(fileExt)
+	matcher.Run(searchDir, srcStr)
 }
 
 
 
 func main() {
-	//Replace(".", ",main", "zet", "cpp")
+	searchDirStrPtr := flag.String("search", "", " Directory to search")
+	patternStrPtr := flag.String("pattern", "", "Search pattern")
+	fileExtStrPtr := flag.String("ext", "", "file exts from which to search")
+	replaceStrPtr := flag.String("replace", "", "The new replcae str")
 
-	Match(".", "zet")
+	flag.Parse()
+
+
+	if *searchDirStrPtr == "" {
+		fmt.Println("Must specify a search diirectory")
+		fmt.Println("usage: -search")
+		os.Exit(1)
+	}
+
+
+	if *patternStrPtr == "" {
+		fmt.Println("Must specify a pattern")
+		fmt.Println("usage: -pattern")
+		os.Exit(1)
+	}
+
+
+	if *fileExtStrPtr == "" {
+		fmt.Println("Must specify a file ext")
+		fmt.Println("usage: -ext ")
+		os.Exit(1)
+	}
+
+	if *replaceStrPtr != "" {
+		Replace(*searchDirStrPtr, *patternStrPtr, *replaceStrPtr, *fileExtStrPtr)
+		return
+	}
+
+	Match(*searchDirStrPtr, *patternStrPtr, *fileExtStrPtr)
 
 }
